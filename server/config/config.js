11c534260de8cecb7e686fed63fb5689d03027f6
Guido Perman <guidoperman@gmail.com>
2020-01-23 09:21:00 -0800

    variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -13,7 +13,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev'){    
     urlDB='mongodb://localhost:27017/cafe'
 }else{
-    urlDB='mongodb+srv://GuidoP:31g971u226@cluster0-onleg.mongodb.net/cafe'
+    urlDB=process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
