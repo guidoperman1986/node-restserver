32c2c1a4fd89a8c42f4b056b98d8bcc97edd02ce
Guido Perman <guidoperman@gmail.com>
2020-02-07 12:15:30 -0800

    Subida de imagenes para productos y usuarios listo

--- server/middelwares/autenticacion.js
+++ server/middelwares/autenticacion.js
@@ -35,7 +35,24 @@ let verificaAdminRole = (req,res,next) => {
     }
 }
 
+function verificaTokenImg(req,res,next){
+    let token = req.query.token;
+
+    jwt.verify(token,process.env.SEED,(err,decoded)=>{
+        if (err){
+            return res.status(401).json({
+                ok:false,
+                err:'token no valido'
+            })
+        }
+
+        req.usuario = decoded.usuario;        
+        next();
+    })
+}
+
 module.exports = {
     verificaToken,
-    verificaAdminRole
+    verificaAdminRole,
+    verificaTokenImg
 }
